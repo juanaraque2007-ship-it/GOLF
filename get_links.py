aed47668ad4006bf430e5d878fd84854ee3a17e0
Juan <juanaraque2007@gmail.com>
2026-05-21 10:24:48 +0200

    Delete get_links.py

--- get_links.py
+++
@@ -1,9 +0,0 @@
-import urllib.request, re
-try:
-    html = urllib.request.urlopen('https://www.fedgolfclm.com/informacion-fgclm-ley-de-transparencia/').read().decode('utf-8')
-    matches = re.findall(r'<a.*?href=[\'\"]([^\'\"]+\.pdf)[\'\"][^>]*>(.*?)</a>', html, flags=re.IGNORECASE|re.DOTALL)
-    for m in matches:
-        text = re.sub(r'<[^>]+>', '', m[1]).strip()
-        print(f'{text} -> {m[0]}')
-except Exception as e:
-    print(e)
